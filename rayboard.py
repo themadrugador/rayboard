5adb6882e00454b0cf8e7bf4a61c0a66ac07570c
themadrugador <cannjohn@gmail.com>
2020-08-01 19:39:24 -0400

    Update rayboard.py

--- rayboard.py
+++ rayboard.py
@@ -29,6 +29,37 @@ r = requests.get(url)
 mydata = quandl.get("FRED/GDP")
 '''
 #%%
+df = pd.DataFrame()
 
-s = fred.get_series('SP500', observation_start='1/31/2014')
-s.tail()
+
+def get_FRED(name, series, start_date):
+    print(name + ' ' + series)
+    df_i = pd.DataFrame()
+    s = fred.get_series(series, observation_start = start_date)
+    data = {'Metric': name, 'Source': 'FRED ' + series, 'Data':[s]}
+    df_i = pd.DataFrame(data)
+    return df_i
+
+FRED_metrics = {'CPI': 'CPIAUCSL', 
+                'Unemployment': 'UNRATE',
+                'PCE': 'PCE',
+                'Personal Income': 'PI',
+                'Personal Saving Rate': 'PSAVERT',
+                'M2': 'M2',
+                'FED Assets': 'WALCL',
+                'Federal Debt': 'MVGFD027MNFRBDAL',
+                'Federal Deficit': 'MTSDS133FMS',
+                'US Dollar': 'DTWEXAFEGS',
+                '10yr Treasury': 'DGS10'}
+
+start_date = '1/1/2019'
+for metric in FRED_metrics:
+    df = df.append(get_FRED(metric,FRED_metrics[metric],start_date))
+df.reset_index(inplace = True, drop = True)
+
+#%% Plot metrics
+for i in range(len(df)):
+    plt.figure()
+    df.loc[i,'Data'].plot()
+    plt.title(df.loc[i,'Metric'])
+    
